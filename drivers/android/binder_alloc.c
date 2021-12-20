65eedcaf2eda54e4a46a0ff2dfbf905d93de4afc
Todd Kjos <tkjos@google.com>
2021-12-20 11:01:50 -0800

    AUTO: UPSTREAM: binder: fix async_free_space accounting for empty parcels (conflict resolved)

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -671,7 +671,12 @@ static void binder_free_buf_locked(struct binder_alloc *alloc,
 	BUG_ON(buffer->user_data > alloc->buffer + alloc->buffer_size);
 
 	if (buffer->async_transaction) {
+<<<<<<< HEAD
 		alloc->free_async_space += buffer_size;
+=======
+		alloc->free_async_space += buffer_size + sizeof(struct binder_buffer);
+
+>>>>>>> 7aedba616cb4 (UPSTREAM: binder: fix async_free_space accounting for empty parcels)
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC_ASYNC,
 			     "%d: binder_free_buf size %zd async free %zd\n",
 			      alloc->pid, size, alloc->free_async_space);
