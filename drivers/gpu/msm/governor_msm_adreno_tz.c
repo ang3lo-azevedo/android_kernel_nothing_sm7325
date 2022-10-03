e20a5645380dd5a614b061543416b758fc6eb61d
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-10-03 17:17:31 +0530

    msm: kgsl: Skip msm-adreno-tz algorithm when single power level in use
    msm-adreno-tz governor updates performance counters which are needed for
    msm-adreno-tz and gpubw_mon governors. When single power level is enabled,
    update stats and return without running msm-adreno-tz algorithm to save
    cpu cycles. These updated stats can be used by gpubw_mon governor.
    
    (cherry picked from commit 06af98a4f5e07900dbca85e6b5fd1b78fceaa06f)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I7b1cdbcf61301e5e5f5cb31d50effa43b9e09e20
    (cherry picked from commit 52e32400bda6271c5b878494a7bc60349cd2b0d6)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    (cherry picked from commit c70b074e511f96bf00a6f430a8f56cabfbdb7b09)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- drivers/gpu/msm/governor_msm_adreno_tz.c
+++ drivers/gpu/msm/governor_msm_adreno_tz.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2010-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2010-2020, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/errno.h>
@@ -14,9 +14,6 @@
 #include <linux/mm.h>
 #include <linux/qcom_scm.h>
 #include <asm/cacheflush.h>
-#ifdef CONFIG_QGKI
-#include <drm/drm_refresh_rate.h>
-#endif
 #include <linux/qtee_shmbridge.h>
 
 #include "../../devfreq/governor.h"
@@ -403,13 +400,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 			priv->bin.busy_time > CEILING) {
 		val = -1 * level;
 	} else {
-#ifdef CONFIG_QGKI
-		unsigned int refresh_rate = dsi_panel_get_refresh_rate();
-
-		if (refresh_rate > 60)
-			priv->bin.busy_time = priv->bin.busy_time * refresh_rate / 60;
-#endif
-
 		val = __secure_tz_update_entry3(level, priv->bin.total_time,
 			priv->bin.busy_time, context_count, priv);
 	}
