7b4708bbe7799c0e00312e78f2600680bb44744c
Alexander Winkowski <dereference23@outlook.com>
2024-05-19 11:46:03 +0000

    sched: Add missing last_enqueue_ts update
    Change-Id: I31b59ed8182cea6bfbb3da15186357b0027e48a5
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3210,6 +3210,9 @@ void wake_up_new_task(struct task_struct *p)
 	post_init_entity_util_avg(p);
 
 	mark_task_starting(p);
+#ifdef CONFIG_SPRD_ROTATION_TASK
+	p->last_enqueue_ts = sched_ktime_clock();
+#endif
 	activate_task(rq, p, ENQUEUE_NOCLOCK);
 	trace_sched_wakeup_new(p);
 	check_preempt_curr(rq, p, WF_FORK);
