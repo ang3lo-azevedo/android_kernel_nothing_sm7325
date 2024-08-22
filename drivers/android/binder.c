5936d5ea1d13929d2dec4d694b51e348cbcf45d0
Carlos Llamas <cmllamas@google.com>
2024-08-22 18:23:52 +0000

    AUTO: FROMLIST: binder: fix UAF caused by offsets overwrite (conflict resolved)

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3726,9 +3726,13 @@ static void binder_transaction(struct binder_proc *proc,
 		copy_size = object_offset - user_offset;
 		if (copy_size && (user_offset > object_offset ||
 <<<<<<< HEAD
+<<<<<<< HEAD
 				object_offset > tr->data_size ||
 =======
 >>>>>>> b007cadee64a (UPSTREAM: binder: avoid potential data leakage when copying txn)
+=======
+				object_offset > tr->data_size ||
+>>>>>>> f4e5b5151e55 (FROMLIST: binder: fix UAF caused by offsets overwrite)
 				binder_alloc_copy_user_to_buffer(
 					&target_proc->alloc,
 					t->buffer, user_offset,
