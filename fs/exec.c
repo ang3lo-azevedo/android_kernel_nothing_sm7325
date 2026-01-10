3c6ede012edc65ae9ae1fc1d0492566412f851b1
William24hmar <Williamhmar24@gmail.com>
2026-01-10 23:02:20 +0530

    Revert "susfs: Fix compilation for k5.4"
    This reverts commit 6a3a6b5b682e58c2ce7f6ebdabef9947a3436047.

--- fs/exec.c
+++ fs/exec.c
@@ -63,7 +63,10 @@
 #include <linux/oom.h>
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
-
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#include "../drivers/kernelsu/ksu.h"
+#endif
 #include <linux/uaccess.h>
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 #include <linux/susfs_def.h>
@@ -71,14 +74,16 @@
 
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
-
 #include <trace/events/task.h>
 #include "internal.h"
 
 #include <trace/events/sched.h>
 
 int suid_dumpable = 0;
-
+extern bool ksu_su_compat_enabled;
+extern bool ksu_execveat_hook;
+extern bool susfs_is_boot_completed_triggered;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
@@ -1768,6 +1773,19 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted()) || !ksu_su_compat_enabled) {
+		goto orig_flow;
+	}
+
+	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	}
+
+orig_flow:
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 	if (likely(susfs_is_current_proc_umounted())) {
@@ -1926,6 +1944,17 @@ out_ret:
 		putname(filename);
 	return retval;
 }
+
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_execveat_hook __read_mostly;
+extern bool ksu_su_compat_enabled __read_mostly;
+extern bool susfs_is_boot_completed_triggered __read_mostly;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
+				void *envp, int *flags);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
