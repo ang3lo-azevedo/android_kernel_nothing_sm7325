189607d94ca6dd137d4924e25d985ecf0a1dfe95
William24hmar <Williamhmar24@gmail.com>
2026-01-20 01:58:38 +0800

    Fix trace probe undefined function warnings

--- kernel/trace/trace_kprobe.c
+++ kernel/trace/trace_kprobe.c
@@ -1176,6 +1176,12 @@ fetch_store_string_user(unsigned long addr, void *dest, void *base)
 }
 
 static nokprobe_inline int
+probe_mem_read(void *dest, void *src, size_t size)
+{
+	return probe_kernel_read_strict(dest, src, size);
+}
+
+static nokprobe_inline int
 probe_mem_read_user(void *dest, void *src, size_t size)
 {
 	const void __user *uaddr =  (__force const void __user *)src;
