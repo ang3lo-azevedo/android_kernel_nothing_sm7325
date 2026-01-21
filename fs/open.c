3930335456a9f77f8169c5a089966b906ebd339d
Madara273 <ravenhoxs@gmail.com>
2026-01-21 11:56:33 +0000

    Revert: drivers: staging: Import inline KernelSU and SUSFS
    (cherry picked from commit 29e53f8c4763f2d8ece38e087285367bd9640987)
    (cherry picked from commit 3b42cc1c6fd73428d22f5568c3f24a0a749a8012)
    (cherry picked from commit 653a4b73f9e1f973d82d52ca73b81a413e1c19f1)
    (cherry picked from commit e5a8a98a5b01d57cfe4e7a4cbbae1b55c743b664)
    (cherry picked from commit d4c47a644ea1fbf669bf698db509706a2c97eb71)
    (cherry picked from commit 75d0490e2f8e2b30f460ab579ed37e81106846b1)
    (cherry picked from commit 04c0740c3bc98acd600bca39b3a194292d6a9c02)
    (cherry picked from commit 35c3e6a7fba010927151f8c7615a8956dbe54e56)
    (cherry picked from commit cd4c125286e7c9724f91a39feb25b2015e224355)
    (cherry picked from commit bb70f7e641ff1f99a31496c174ca35bb2e1ee96b)
    (cherry picked from commit 6e80428f06cc96ccf80e316f069c966ca41bea6a)
    (cherry picked from commit 034f05caf728d5e244d4d9c022a39a752a27f968)
    (cherry picked from commit 690f8b8b2b9561a04746997c9e3648283e51247b)
    (cherry picked from commit 88e3845686b7938b0e14b5bd3d2274f95148ed71)
    (cherry picked from commit 5d62ef208a04b8a044af70a56ad43b1bac2d1b22)
    (cherry picked from commit fd7b8e0be81dd217249107be5ad268e3ef67dc22)
    (cherry picked from commit 9582cf6bd2cffeb7c8febd1f7b9ab4da75813685)
    (cherry picked from commit 0c056cf3d49011f7dc55bbf4bf9b9bb0cb8fb39d)
    (cherry picked from commit de90fe84c21f355b1d5ef47c75fd9ca7889c0b1c)
    Change-Id: Id4f64b376ebfdf6ea37a5ffeadd0bcf814953cae
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Lv Ruyi <lv.ruyi@zte.com.cn>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Souradeep Chowdhury <quic_schowdhu@quicinc.com>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -345,7 +345,6 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
-
 long do_faccessat(int dfd, const char __user *filename, int mode)
 {
 	const struct cred *old_cred;
@@ -441,16 +440,8 @@ out:
 	return res;
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			                    int *flags);
-#endif
-
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-#endif
 	return do_faccessat(dfd, filename, mode);
 }
 
