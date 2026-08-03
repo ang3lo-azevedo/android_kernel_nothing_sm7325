d6e9bfb2b6a11d68c636e748a04e66c7e268ed32
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-03 17:35:34 +0100

    focaltech: exclude FB notifier blocks when CONFIG_DRM is also defined

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -1546,7 +1546,7 @@ static void fts_resume_work(struct work_struct *work)
     fts_ts_resume(ts_data->dev);
 }
 
-#if defined(CONFIG_FB)
+#if defined(CONFIG_FB) && !defined(CONFIG_DRM)
 static int fb_notifier_callback(struct notifier_block *self,
                                 unsigned long event, void *data)
 {
@@ -1888,7 +1888,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
     ts_data->pm_suspend = false;
 #endif
 
-#if defined(CONFIG_FB)
+#if defined(CONFIG_FB) && !defined(CONFIG_DRM)
     ts_data->fb_notif.notifier_call = fb_notifier_callback;
     ret = fb_register_client(&ts_data->fb_notif);
     if (ret) {
@@ -1978,7 +1978,7 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
     if (ts_data->ts_workqueue)
         destroy_workqueue(ts_data->ts_workqueue);
 
-#if defined(CONFIG_FB)
+#if defined(CONFIG_FB) && !defined(CONFIG_DRM)
     if (fb_unregister_client(&ts_data->fb_notif))
         FTS_ERROR("[FB]Error occurred while unregistering fb_notifier.");
 #elif defined(CONFIG_DRM)
